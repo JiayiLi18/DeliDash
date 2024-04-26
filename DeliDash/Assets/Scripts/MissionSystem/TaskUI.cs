1193780255f617f48b1d54f6dde7decb1ebabb06
MareImbrium2J <792618256@qq.com>
2024-04-26 20:53:58 +0200

    任务系统

---
+++ DeliDash/Assets/Scripts/MissionSystem/TaskUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskUI : MonoBehaviour
+{
+    public Text taskStatusText;
+
+    public void UpdateTaskUI(Task task)
+    {
+        string taskStatus = "Task Status:\n";
+        if (task.isPickedUp)
+        {
+            taskStatus += "Takeaway Picked Up\n";
+            if (task.isDelivered)
+            {
+                taskStatus += "Takeaway Delivered";
+            }
+            else
+            {
+                taskStatus += "On the way to Delivery";
+            }
+        }
+        else
+        {
+            taskStatus += "Takeaway Not Picked Up";
+        }
+        taskStatusText.text = taskStatus;
+    }
+}
